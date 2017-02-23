6aefbd79aa7344a4d73c9b00649c8343741ae018
jennifersalas <jennysalas@me.com>
2017-02-22 22:41:12 -0800

    Added stuff

---
+++ extras.c
@@ -0,0 +1,30 @@
+//
+//  extras.c
+//  ECS 30 Work
+//
+//  Created by Jennifer Salas on 11/21/16.
+//  Copyright © 2016 Jennifer Salas. All rights reserved.
+//
+
+#include "extras.h"
+
+
+//
+//char* read_line(FILE *fp) {
+//
+//    char *line= (char*)malloc(sizeof(char));
+//    int num_chars = 0;
+//
+//    do {
+//
+//        line = (char*) realloc(line, (num_chars + 1)* sizeof(char));
+//        fscanf(fp, "%c", &line[num_chars]);
+//        num_chars++;
+//
+//    } while (line[num_chars-1] != '\n');
+//
+//    line[num_chars-1] = '\0';
+//
+//    return line;
+//
+//}
---
+++ extras.h
@@ -0,0 +1,14 @@
+//
+//  extras.h
+//  ECS 30 Work
+//
+//  Created by Jennifer Salas on 11/21/16.
+//  Copyright © 2016 Jennifer Salas. All rights reserved.
+//
+
+#ifndef extras_h
+#define extras_h
+
+#include <stdio.h>
+
+#endif /* extras_h */
